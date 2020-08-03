6158302ede54c05eff50cd90056dc3f994c4a561
NikitinMikhail <36502527+NikitinMikhail@users.noreply.github.com>
2020-08-03 18:09:55 +0300

    Update IPRB.py

--- level4/IPRB.py
+++ level4/IPRB.py
@@ -1,1 +1,10 @@
+line = open('IPRB.txt', 'r').readline().strip()
 
+dom, het, rec = [int(i) for i in line.split()]
+
+total_comb = (dom + het + rec)*(dom + het + rec - 1)
+dom_comb = dom * (dom + het + rec - 1) + het * (dom + 0.75 * (het - 1) + 0.5 * rec) + rec * (dom + 0.5 * het)
+
+prob = dom_comb / total_comb
+
+print(prob)
