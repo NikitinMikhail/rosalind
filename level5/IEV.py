456075c2d943798064f514d7719a9c89066a21bc
NikitinMikhail <mishniktl165@gmail.com>
2020-09-23 23:58:08 +0300

    'IEV text'

--- level5/IEV.py
+++ level5/IEV.py
@@ -1,3 +1,7 @@
-line = open('IEV.TXT', 'r').readline().strip()
+import numpy
+read_list = open('IEV.TXT', 'r').readline().strip().split()
 
-items = map(line.split(),
+parents_number = [int(item) for item in read_list]
+dom_ratio = [1, 1, 1, 0.75, 0.5, 0]
+expected_dom = 2 * numpy.dot(parents_number, dom_ratio)
+print(expected_dom)
