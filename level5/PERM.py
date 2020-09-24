5047a17f57ed67ede2a63d69b590e9c10abcd7d2
NikitinMikhail <mishniktl165@gmail.com>
2020-09-24 10:06:47 +0300

    'Couple of problems added'

--- level5/PERM.py
+++ level5/PERM.py
@@ -1,7 +1,13 @@
 import common
 import math
+import itertools
 
 input_number = int(common.one_line_reader('PERM.txt'))
-print(input_number)
 total_perm = math.factorial(input_number)
-print(total_perm)
+num_list = list(range(1, input_number + 1))
+iter_list = list(itertools.permutations(num_list))
+
+with open('PERM_result.txt', 'w') as f:
+    print(total_perm, file=f)
+    for perm in iter_list:
+        print(' '.join(map(str, [item for item in perm])), file=f)
