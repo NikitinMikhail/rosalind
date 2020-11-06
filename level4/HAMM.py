87ec4bdc560d53349f64b9e7355b64b9d43ed5f8
NikitinMikhail <mishniktl165@gmail.com>
2020-11-06 10:25:41 +0300

    PDST added

--- level4/HAMM.py
+++ level4/HAMM.py
@@ -1,17 +1,17 @@
-import re
+def hamming_distance(strand1, strand2):
+    mismatches = 0
+    counter = 0
 
-strand_lines = open('HAMM.txt', 'r').readlines()
+    for letter in strand1:
+        if letter != strand2[counter]:
+            mismatches += 1
+        counter += 1
+    return mismatches
 
-strand1 = strand_lines[0].strip()
-strand2 = strand_lines[1].strip()
-mismatches = 0
-counter = 0
 
-for letter in strand1:
-	if letter != strand2[counter]:
-		mismatches += 1
-	counter += 1
+if __name__ == '__main__':
+    strand_lines = open('HAMM.txt', 'r').readlines()
 
-#print(strand1)
-#print(strand2)
-print(mismatches)
+    strand1 = strand_lines[0].strip()
+    strand2 = strand_lines[1].strip()
+    print(hamming_distance(strand1, strand2))
